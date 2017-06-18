3b9efb05a357cf975c19ee2c150becf95dd50c0a
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-06-18 15:03:41 +0200

    Dead code

--- src/js/modules/board.js
+++ src/js/modules/board.js
@@ -46,9 +46,6 @@ const Board = (function() {
 		const middleY = canvas.getHeight() / 2;
 		const leftTopX = middleX - getBoard().getWidth() / 2 * scale;
 		const leftTopY = middleY - getBoard().getHeight() / 2 * scale;
-		graphics.fillStyle = 'lightgreen';
-		graphics.fillRect(leftTopX, leftTopY, board.getWidth() * scale, board.getHeight() * scale);
-		graphics.fillStyle = 'black';
 		for (let y = 0; y < board.getHeight(); y++) {
 			for (let x = 0; x < board.getWidth(); x++) {
 				board.getField(x, y).draw(graphics, leftTopX + x * scale, leftTopY + y * scale, scale);
